5a055e9993d9ee160a81cea8dc47b801f04b9cb7
fisherwangcn <fisherwangcn@163.com>
2019-09-25 15:16:01 +0800

    123

---
+++ src/main/java/com/example/testuserservice/TestUserServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.testuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TestUserServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestUserServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+class MyController{
+	@RequestMapping(value = "/user", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public Object getUser(){
+		return "user1";
+	}
+}
---
+++ src/test/java/com/example/testuserservice/TestUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testuserservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUserServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
