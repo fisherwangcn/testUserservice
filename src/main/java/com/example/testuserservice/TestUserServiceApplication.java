dbdf7274d72b92f64a9a2241e7ecb5ade994dbaf
fisherwangcn <fisherwangcn@163.com>
2019-10-17 15:20:36 +0800

    first commit

--- src/main/java/com/example/testuserservice/TestUserServiceApplication.java
+++ src/main/java/com/example/testuserservice/TestUserServiceApplication.java
@@ -20,6 +20,6 @@ class MyController{
 	@RequestMapping(value = "/user", produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public Object getUser(){
-		return "branch master user1 ----------second commit";
+		return "branch mybranch user1 ----------first commit";
 	}
 }
